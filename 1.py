06c8380c3e013402d675f37bfcbb8de1ac2022e2
tq <wangtie@oregonstate.edu>
2023-08-16 10:44:25 -0700

    updates 8.16.1

---
+++ 1.py
@@ -0,0 +1,14 @@
+import DevEv1
+
+# import argparse
+# parser = argparse.ArgumentParser()
+# parser.add_argument('--keypoints',default='./DevEvR/viz_3dpose8/data_3d_DevEv_S07_04_Sync.npy')
+# parser.add_argument('--type',type=int,default=0)
+# parser.add_argument('--hand',type=int,default=1)
+# parser.add_argument('--foot',type=int,default=1)
+# args = parser.parse_args()
+
+def test_xjk():
+    # print(args.filename)
+    DevEv1.run()
+test_xjk()
