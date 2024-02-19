62fff6577b3cdde8046b4c0b7153238bde5a0e30
tqosu <wangtie@oregonstate.edu>
2024-02-19 12:48:50 -0800

    Flex_240219.csv, S68 57 onset time correction

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import FlexUI
 import yaml
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument('--csv_name',default='Flex_240215.csv')
+parser.add_argument('--csv_name',default='Flex_240219.csv')
 args = parser.parse_args()
 
 def test_xjk():
